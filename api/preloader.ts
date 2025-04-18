476b8461f4c396504b4777623ab6e0ff36a5b57c
Blackbrv <luthfiprtm@gmail.com>
2025-04-18 09:46:02 +0700

    Changes

--- api/preloader.ts
+++ api/preloader.ts
@@ -1,7 +1,7 @@
 import { promises as fs } from "fs";
 import path from "path";
 
-const FILE_PATH = path.join(process.cwd(), "preloaders.json");
+const FILE_PATH = path.join(process.cwd(), "data.json");
 
 export async function GET() {
   const file = await fs.readFile(FILE_PATH, "utf-8");
